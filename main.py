6037042b3f5b4ec4eebb10cb1fea85d70ce684ea
KimHaejoong1 <hjkim2124@gmail.com>
2025-07-14 21:55:59 +0900

    fix: UI 전달받지 못해 실행이 안되는 오류 해결

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 import sys
-from chess.ui.pygame_ui import PygameUI
 from chess.game_controller import GameController
 
 def main():
-    # UI 초기화
-    ui = PygameUI()
-    
-    # 게임 컨트롤러 초기화
-    game_controller = GameController(ui)
+    # 게임 컨트롤러 초기화 (UI는 내부에서 생성됨)
+    game_controller = GameController()
     
     # 게임 실행
     game_controller.run()
